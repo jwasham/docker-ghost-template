a1074758226c2ce507605912de4c3eb0d3a2dc48
jwasham <john@readyprompt.com>
2016-10-03 11:13:00 -0700

    Updating for Ghost 0.11.1

--- ghost/copy/entrypoint.sh
+++ ghost/copy/entrypoint.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 set -e
 
 if [[ "$*" == npm*start* ]]; then
@@ -10,8 +9,9 @@ if [[ "$*" == npm*start* ]]; then
         sleep 30
     fi
 
-	for dir in "$GHOST_SOURCE/content"/*/; do
-		targetDir="$GHOST_CONTENT/$(basename "$dir")"
+	baseDir="$GHOST_SOURCE/content"
+	for dir in "$baseDir"/*/ "$baseDir"/themes/*/; do
+		targetDir="$GHOST_CONTENT/${dir#$baseDir/}"
 		mkdir -p "$targetDir"
 		if [ -z "$(ls -A "$targetDir")" ]; then
 			tar -c --one-file-system -C "$dir" . | tar xC "$targetDir"
@@ -19,7 +19,6 @@ if [[ "$*" == npm*start* ]]; then
 	done
 
 	if [ ! -e "$GHOST_CONTENT/config.js" ]; then
-
 		sed -r '
 			s/127\.0\.0\.1/0.0.0.0/g;
 			s!path.join\(__dirname, (.)/content!path.join(process.env.GHOST_CONTENT, \1!g;
