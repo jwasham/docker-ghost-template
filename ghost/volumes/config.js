c6eab560a84b92114bfeaa3138930d00a7612ebd
jwasham <john@readyprompt.com>
2016-10-03 11:59:51 -0700

    Working on S3 storage upgrade.

--- ghost/volumes/config.js
+++ ghost/volumes/config.js
@@ -38,8 +38,8 @@ config = {
         },
 
         storage: {
-            active: 's3',
-            's3': {
+            active: 'ghost-storage-adapter-s3',
+            'ghost-storage-adapter-s3': {
                 accessKeyId:     process.env.S3_ACCESS_KEY_ID,
                 secretAccessKey: process.env.S3_ACCESS_SECRET_KEY,
                 bucket:          process.env.S3_BUCKET_NAME,
